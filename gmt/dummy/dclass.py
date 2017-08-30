8beb86b223d86b43471f1e7df5726060688eec1a
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-08-30 17:11:10 +0100

    added a DClass class

---
+++ gmt/dummy/dclass.py
@@ -0,0 +1,17 @@
+class DClass:
+    """This is just a dummy class."""
+
+    def __init__(self):
+        """Initialise a dummy object."""
+        pass
+
+    def act(self, cmd):
+        """Act with given command.
+
+        :param cmd: command to act upon
+        :type cmd: str
+        :raises ValueError: if `cmd` is ``None``
+        """
+        if cmd is None:
+            raise ValueError('There is no None act.')
+        print('Acting {}'.format(cmd))
