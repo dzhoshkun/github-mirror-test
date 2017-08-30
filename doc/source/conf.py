b32f39613ebe96dbc7791c782392bdf7306da666
Dzhoshkun Ismail Shakir <d.shakir@ucl.ac.uk>
2017-08-30 18:06:14 +0100

    refactored module path appending using a variable for debugging

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -18,9 +18,11 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
-print('module path is {}'.format(os.path.abspath('..')))
+
+module_path = os.path.abspath('..')
+print('module path is {}'.format(module_path))
+sys.path.insert(0, module_path)
 
 
 # -- General configuration ------------------------------------------------
